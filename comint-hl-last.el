ecb82d411fef8c41b2a676f6c556e320fa24171b
Jan Rehders <cmdkeen@gmx.de>
2015-02-12 23:37:31 +0000

    v0.2, improved robustness
    - Mode can only be enabled in major modes derived from comint-mode.
    - Toggling global mode will add/remove display in all buffers.
    - Do not highlight last input line.

--- comint-hl-last.el
+++ comint-hl-last.el
@@ -1,32 +1,53 @@
 ;;; comint-hl-last.el --- Highlight output of last command in fringe
-
+;;
 ;; Copyright 2015 Jan Rehders
 ;; 
 ;; Author: Jan Rehders <jan@sheijk.net>
-;; Version: 0.1
-
+;; Version: 0.2
+;;
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
-
+;;
 ;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
-
+;;
 ;;; Commentary:
 ;;
 ;; This defines a minor mode which will highlight the output of the last command
 ;; by a line in the fringe. It works with all comint derived modes. Call
 ;; comint-hl-last-mode or global-comint-hl-last-mode and highlighting will
 ;; appear in shells, gud, etc.
-
+;;
+;;; Changelog
+;;
+;; v0.2, 2015-02-12
+;; - Mode can only be enabled in major modes derived from comint-mode.
+;; - Toggling global mode will add/remove display in all buffers.
+;; - Do not highlight last input line.
+;;
+;;; Tests
+;;
+;; For now only be hand. Check the following:
+;; - Basic functionality
+;;   - M-x, shell, M-x comint-hl-mode, enter several commands and check that the
+;;     output of the last command gets highlighted each time in the fringe.
+;;   - M-x, comint-hl-mode, check that highlighting gets removed.
+;;
+;; - Global mode
+;;   - M-x, shell, pwd, ls, M-x, rename-buffer, *shell2*, M-x, shell, ls, pwd,
+;;     M-x, global-comint-hl-last-mode, check highlighting is in both shells
+;;   - M-x, global-comint-hl-last-mode, check if highlighting gets removed in
+;;     both shells.
+;;
 ;;; Code:
 
 (require 'fringe-helper)
@@ -63,24 +84,44 @@ Will be added to `comint-output-filter-functions' when mode is active.
 ARG is ignored."
   (comint-hl-last-remove)
   (setq comint-hl-last-highlight
-        (fringe-helper-insert-region comint-last-input-start
+        (fringe-helper-insert-region (save-excursion
+                                       (goto-char comint-last-input-start)
+                                       (next-line)
+                                       (beginning-of-line 1)
+                                       (point))
                                      (point-max)
                                      'comint-hl-last-marker
                                      'left-fringe
-                                     'comint-hl-last-marker-face)))
+                                     'comint-hl-last-marker-face))
+  nil)
 
 (defun comint-hl-last-toggle (global)
   "Toggle either global-comint-hl-last-mode or comint-hl-last-mode.
 
 GLOBAL decides between global and local mode."
-  (if (if global global-comint-hl-last-mode comint-hl-last-mode)
-      (progn
-        (add-hook 'comint-output-filter-functions 'comint-hl-last-update t (not global))
-        (unless global
-          (comint-hl-last-update "")))
-    (unless global
-      (comint-hl-last-remove))
-    (remove-hook 'comint-output-filter-functions 'comint-hl-last-update (not global))))
+  (let ((turned-on (if global global-comint-hl-last-mode comint-hl-last-mode)))
+    (cond
+     ((and global turned-on)
+      (add-hook 'comint-output-filter-functions 'comint-hl-last-update t nil)
+      (dolist (buffer (buffer-list))
+        (with-current-buffer buffer
+          (when (derived-mode-p 'comint-mode)
+            (comint-hl-last-update "")))))
+
+     ((and global (not turned-on))
+      (remove-hook 'comint-output-filter-functions 'comint-hl-last-update nil)
+      (dolist (buffer (buffer-list))
+        (with-current-buffer buffer
+          (when (derived-mode-p 'comint-mode)
+            (comint-hl-last-remove)))))
+
+     ((and (not global) turned-on)
+      (add-hook 'comint-output-filter-functions 'comint-hl-last-update t t)
+      (comint-hl-last-update ""))
+
+     ((and (not global) (not turned-on))
+      (comint-hl-last-remove)
+      (remove-hook 'comint-output-filter-functions 'comint-hl-last-update (not global))))))
 
 ;;;###autoload
 (define-minor-mode comint-hl-last-mode
